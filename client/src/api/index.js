7b665a7a6f604459a45194b4157913f5056e6781
arthurR <92532853+ArthurRaimann@users.noreply.github.com>
2023-02-09 09:35:00 +0100

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = "https://our-memories-project-25.herokuapp.com/posts"
+const url = "https://arthur-memories-api.herokuapp.com/posts"
 
 export const fetchPosts = () => axios.get(url)
 export const createPost = (newPost) => axios.post(url, newPost)
